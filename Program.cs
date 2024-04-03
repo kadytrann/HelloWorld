90c31eddd5822c368a243a1fd1621d21df864b12
Kevin Tran <Kevin Tran@KEEVON>
2024-04-02 19:04:10 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // To get your project setup with GitHub
+            //1. Open View -> Git Changes
+
+
+            // To comment code, do double forward slashs // 
+            // console gives us access to working with the console
+            // CODE IS CASE SENSITIVE
+            // Dot, ., is used to access the different ways to interact with your code
+            //Parentheses are used to RUN our code, and pass in different arguments
+            // semicolon, the end of a single statement
+            // curly braces define code blocks
+
+            // "Hello, World"
+            // Double quotes tell the computer that everything in between is a STRING
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine();
+
+            // Compile time error
+            // is an error that VS will warn you about, that there is improper syntax someplace. Needs to be fixed before you can build your code.
+            // Errors in the Error list will "Help" you find the problem
+            
+        } // End of main
+    } // End of Class
+} // End of Name Space
